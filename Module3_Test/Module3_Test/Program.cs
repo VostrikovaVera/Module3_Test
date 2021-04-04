9a646a065461079da481e194e001f76a68446194
Vira Vostrikova <vvostrikova@csod.com>
2021-04-04 22:28:43 +0300

    Initial commit

---
+++ Module3_Test/Module3_Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Module3_Test
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
